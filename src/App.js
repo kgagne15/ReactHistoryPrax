7e1e1f727d4576cbdf5bf6ca51b3c6560bba251c
kgagne15 <kristinagagne@ymail.com>
2022-10-26 07:05:12 -0400

    functionality added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import JokeList from "./JokeList";
+import JokeListClass from "./ClassReact/JokeListClass"
+
+function App() {
+  return (
+    <div className="App">
+      <JokeListClass />
+      {/* <JokeList /> */}
+    </div>
+  );
+}
+
+export default App;
